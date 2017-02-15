03fe3251cacd5e908d83a69035cdc9b356c9b680
elmar <e.plantenga@gmail.com>
2017-02-15 17:28:27 +0100

    fixes for php5.6 | working on php7.x

--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,9 @@
 <?php
 phpinfo();
+
+$host = 'dbext057604.bytenet.nl';
+$user = 'u057604_funx';
+$password = 'b-PXa0914W(Ee)4K';
+$db = 'db057604_funx';
+
+mysqli_connect($host, $user, $password, $db);
